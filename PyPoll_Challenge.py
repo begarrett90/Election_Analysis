0872816da76ade23406f41d5dcda026d69be4df6
begarrett90 <105942622+begarrett90@users.noreply.github.com>
2022-06-18 13:42:07 -0400

    Updated code to take out unnecessary item.

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -4,7 +4,6 @@
 # Add our dependencies.
 import csv
 import os
-from tkinter import N
 
 # Add a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
